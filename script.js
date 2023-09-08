a7ffe331ddbd2a3c1b0fff407f1beee4788720da
Chill Wu <urdnorn@gmail.com>
2023-09-08 11:45:21 +0800

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+// Function to change the text and color of the paragraph
+function changeText() {
+    const paragraph = document.getElementById("myParagraph");
+    paragraph.innerHTML = "The text has changed!";
+    paragraph.style.color = "red";
+    
+}
+document.querySelector("html").onclick = function () {
+    alert("Ouch! Stop poking me!");
+  };
+  
